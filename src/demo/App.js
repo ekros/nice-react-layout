06bd6236bfe86147c946d67f9ff8fb895aacf285
Eric Ros <ekros@outlook.com>
2018-04-06 10:08:58 +0200

    Add examples page

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,97 +1,280 @@
 import React, { Component } from "react";
 import {
-	HorizontalLayout,
-	VerticalLayout,
-	Panel,
-	Separator,
-	Spacer,
+  HorizontalLayout,
+  VerticalLayout,
+  Panel,
+  Separator,
+  Spacer
 } from "../lib/components/Layout";
 import { View } from "../lib/components/View";
-import { FormLayout, TableLayout } from "../lib/components/Layout/panel_layouts";
-
+import {
+  FormLayout,
+  TableLayout
+} from "../lib/components/Layout/panel_layouts";
 import "./App.css";
 
 class App extends Component {
-	render() {
-		const styles = {
-			collapseButton: {
-				background: "azure",
-				border: 0,
-				float: "right",
-				outline: "none",
-			},
-			exampleContent: {
-				width: "100px",
-				height: "100px",
-				border: "1px solid gray",
-				background: "lightgray",
-				borderRadius: "4px",
-			},
-			tableTop: {
-				width: "100%",
-				height: "100%",
-				background: "rgba(0, 0, 0, 0.5)",
-				textAlign: "center",
-			},
-			tableBottom: {
-				width: "100%",
-				height: "100%",
-				background: "rgba(0, 0, 0, 0.2)",
-				textAlign: "center",
-			},
-		};
-		return (
-			<View>
-				<HorizontalLayout mockup collapseSize="60px">
-					<Panel
-						proportion={1}
-						sidebar
-						collapsible
-						collapseButtonStyle={styles.collapseButton}
-					>
-						<VerticalLayout mockup separatorsRefreshInterval={200}>
-							<Panel fixed fixedHeight={100}>
-								Top nested fixed panel
-							</Panel>
-							<Separator />
-							<Panel proportion={2}>
-								Center nested panel. The separator below has a refresh rate of
-								200ms.
-							</Panel>
-							<Separator />
-							<Panel minHeight={200} proportion={1} centered>
-								<div style={styles.exampleContent}>Example centered content</div>
-							</Panel>
-						</VerticalLayout>
-					</Panel>
-					{/* If you double-click this separator it will give a size of 200px to the left panel */}
-					<Separator defaultDblClickPos={200} />
-					<Panel proportion={2}>
-						<TableLayout
-							topBarComponent={<div style={styles.tableTop}>This is a TableLayout...</div>}
-							bottomTableComponent={
-								<div style={styles.tableBottom}>...with topBarComponent and bottomTableComponent</div>
-							}
-						/>
-					</Panel>
-					{/* Use Spacer to add a blank space to the layout (it may help in positioning other elements) */}
-					<Spacer size={50} />
-					<Panel proportion={1}>
-						{/* You can add a mockup prop for quick table prototyping */}
-						<TableLayout mockup />
-					</Panel>
-					<Separator defaultDblClickPos={200} />
-					<Panel proportion={1}>
-						<FormLayout mockup />
-					</Panel>
-					<Separator />
-					<Panel fixed fixedWidth={300} centered>
-						Fixed panel. I can't be resized.
-					</Panel>
-				</HorizontalLayout>
-			</View>
-		);
-	}
+  render() {
+    const styles = {
+      collapseButton: {
+        background: "azure",
+        border: 0,
+        float: "right",
+        outline: "none"
+      },
+      example: {
+        width: "600px",
+        height: "400px",
+        border: "1px solid gray",
+        borderRadius: "4px",
+        margin: "10px 0px"
+      },
+      github: {
+        width: "32px",
+        height: "32px"
+      },
+      main: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+      },
+      tableTop: {
+        width: "100%",
+        height: "100%",
+        background: "rgba(0, 0, 0, 0.5)",
+        textAlign: "center"
+      },
+      tableBottom: {
+        width: "100%",
+        height: "100%",
+        background: "rgba(0, 0, 0, 0.2)",
+        textAlign: "center"
+      }
+    };
+    return (
+      <div style={styles.main}>
+        <br />
+        <div style={{ width: "600px" }}>
+          <h1 style={{ display: "inline-block" }}>nice-react-layout</h1>
+          <a style={{ float: "right" }} href="https://github.com/ekros/nice-react-layout">
+            <img style={styles.github} src="https://github.com/favicon.ico" alt="github" />
+          </a>
+          <p>A set of React components to create complex flexbox-based layouts without knowing what flexbox is.</p>
+        </div>
+        <br />
+        <div className="pt-card">
+          <h5>Basic example</h5>
+          <pre className="prettyprint">
+            {`
+            <HorizontalLayout mockup>
+              <Panel proportion={1} />
+              <Panel proportion={3} />
+            </HorizontalLayout>
+            `}
+          </pre>
+          <div style={styles.example}>
+            <HorizontalLayout mockup>
+              <Panel proportion={1} />
+              <Panel proportion={3} />
+            </HorizontalLayout>
+          </div>
+        </div>
+        <br />
+        <div className="pt-card">
+          <h5>Nested layouts</h5>
+          <pre className="prettyprint">
+            {`
+              <VerticalLayout mockup>
+                <Panel fixed fixedHeight={50} />
+                <Panel>
+                  <HorizontalLayout mockup>
+                    <Panel />
+                    <Panel proportion={3} />
+                  </HorizontalLayout>
+                </Panel>
+              </VerticalLayout>
+            `}
+          </pre>
+          <div style={styles.example}>
+            <VerticalLayout mockup>
+              <Panel fixed fixedHeight={50} />
+              <Panel>
+                <HorizontalLayout mockup>
+                  <Panel />
+                  <Panel proportion={3} />
+                </HorizontalLayout>
+              </Panel>
+            </VerticalLayout>
+          </div>
+        </div>
+        <br />
+        <div className="pt-card">
+          <h5>Separator</h5>
+          <pre className="prettyprint">
+            {`
+              <VerticalLayout mockup>
+                <Panel fixed fixedHeight={50} />
+                <Panel>
+                  <HorizontalLayout mockup>
+                    <Panel />
+                    <Separator />
+                    <Panel />
+                    <Panel fixed fixedWidth={100} />
+                  </HorizontalLayout>
+                </Panel>
+              </VerticalLayout>
+            `}
+          </pre>
+          <div style={styles.example}>
+            <VerticalLayout mockup>
+              <Panel fixed fixedHeight={50} />
+              <Panel>
+                <HorizontalLayout mockup>
+                  <Panel />
+                  <Separator />
+                  <Panel />
+                  <Panel fixed fixedWidth={100} />
+                </HorizontalLayout>
+              </Panel>
+            </VerticalLayout>
+          </div>
+        </div>
+        <br />
+        <div className="pt-card">
+          <h5>Collapsible sidebar</h5>
+          <pre className="prettyprint">
+            {`
+              <VerticalLayout mockup>
+                <Panel fixed fixedHeight={50} />
+                <Panel>
+                  <HorizontalLayout mockup>
+                    <Panel
+                      sidebar
+                      collapsible
+                      collapseButtonContent="<"
+                      collapseButtonCollapsedContent=">"
+                      collapseButtonStyle={{
+                        background: "white",
+                        border: "1px solid lightgray"
+                      }}
+                    />
+                    <Separator />
+                    <Panel proportion={3} />
+                  </HorizontalLayout>
+                </Panel>
+              </VerticalLayout>
+            `}
+          </pre>
+          <div style={styles.example}>
+            <VerticalLayout mockup>
+              <Panel fixed fixedHeight={50} />
+              <Panel>
+                <HorizontalLayout mockup>
+                  <Panel
+                    sidebar
+                    collapsible
+                    collapseButtonContent="<"
+                    collapseButtonCollapsedContent=">"
+                    collapseButtonStyle={{
+                      background: "white",
+                      border: "1px solid lightgray"
+                    }}
+                  />
+                  <Separator />
+                  <Panel proportion={3} />
+                </HorizontalLayout>
+              </Panel>
+            </VerticalLayout>
+          </div>
+        </div>
+        <br />
+        <div className="pt-card">
+          <h5>Multiple collapsible panels</h5>
+          <pre className="prettyprint">
+            {`
+              <VerticalLayout mockup>
+                <Panel fixed fixedHeight={50} />
+                <Panel>
+                  <HorizontalLayout mockup>
+                    <Panel
+                      sidebar
+                      collapsible
+                      collapseButtonContent="-"
+                      collapseButtonCollapsedContent="+"
+                      collapseButtonStyle={{
+                        background: "white",
+                        border: "1px solid lightgray"
+                      }}
+                    />
+                    <Panel
+                      sidebar
+                      collapsible
+                      collapseButtonContent="-"
+                      collapseButtonCollapsedContent="+"
+                      collapseButtonStyle={{
+                        background: "white",
+                        border: "1px solid lightgray"
+                      }}
+                    />
+                    <Panel
+                      sidebar
+                      collapsible
+                      collapseButtonContent="-"
+                      collapseButtonCollapsedContent="+"
+                      collapseButtonStyle={{
+                        background: "white",
+                        border: "1px solid lightgray"
+                      }}
+                    />
+                  </HorizontalLayout>
+                </Panel>
+              </VerticalLayout>
+            `}
+          </pre>
+          <div style={styles.example}>
+            <VerticalLayout mockup>
+              <Panel fixed fixedHeight={50} />
+              <Panel>
+                <HorizontalLayout mockup>
+                  <Panel
+                    sidebar
+                    collapsible
+                    collapseButtonContent="-"
+                    collapseButtonCollapsedContent="+"
+                    collapseButtonStyle={{
+                      background: "white",
+                      border: "1px solid lightgray"
+                    }}
+                  />
+                  <Panel
+                    sidebar
+                    collapsible
+                    collapseButtonContent="-"
+                    collapseButtonCollapsedContent="+"
+                    collapseButtonStyle={{
+                      background: "white",
+                      border: "1px solid lightgray"
+                    }}
+                  />
+                  <Panel
+                    sidebar
+                    collapsible
+                    collapseButtonContent="-"
+                    collapseButtonCollapsedContent="+"
+                    collapseButtonStyle={{
+                      background: "white",
+                      border: "1px solid lightgray"
+                    }}
+                  />
+                </HorizontalLayout>
+              </Panel>
+            </VerticalLayout>
+          </div>
+        </div>
+        <br />
+      </div>
+    );
+  }
 }
 
 export default App;
